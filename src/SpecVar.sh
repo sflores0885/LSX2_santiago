9fae690d31c37197d39484109c556dfef87abc68
Santiago Flores P <santi.flores.prado@gmail.com>
2016-10-08 11:59:14 -0600

    actualizaciones del SpecVar

--- src/SpecVar.sh
+++ src/SpecVar.sh
@@ -20,3 +20,15 @@ fi
 # Quiero ver cuales fueron los argumentos 
 
 
+echo "los parametros del script fueron:"
+echo " "
+VAR1=1
+## Diferencia entre $0 (es un arreglo) y $* (que es un string)
+
+for PARAM in $0
+do
+	echo "El parametro $VAR1 es: $PARAM"
+	let "VAR1=$VAR1+1"
+	echo "VAR1 es :$VAR1"
+done
+
